bbb15ded4ca8b334449fe3e077cd0867d7dc642f
Aaron Hazzard <aaronsploit@gmail.com>
2026-02-12 13:52:56 -0400

    Updated founder's image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,7 +15,7 @@ import MouseFollower3D from '@/components/animations/MouseFollower3D'
 import SmoothScroll from '@/components/ui/SmoothScroll'
 
 // Import Aaron images for pre-rendering
-import aaronImage from '../../public/aaron.png'
+import aaronImage from '../../public/aaron.jpg'
 import aaron2Image from '../../public/aaron2.jpg'
 
 // Generate structured data for the About page
